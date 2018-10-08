82b26a8f854c94a79024bfc11d2837fad8c0b26e
murilobauerc <panchobauer@gmail.com>
2018-10-08 15:27:14 -0300

    deploy

--- src/server.js
+++ src/server.js
@@ -1,16 +1,4 @@
-//Install express server
 const express = require('express');
-const path = require('path');
-
 const app = express();
-
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/personal-api-riot'));
-
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/personal-api-riot/index.html'));
-});
-
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.use(express.static(__dirname + '/src')); //aqui você define onde está o index.html da sua aplicação.
+app.listen(process.env.PORT || 3000);
