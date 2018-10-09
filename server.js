8bb40b2d83ed85f0321ba2d168dacd51f93bda53
murilobauerc <panchobauer@gmail.com>
2018-10-09 11:29:57 -0300

    deploy :shipit:

--- server.js
+++ server.js
@@ -1,4 +1,21 @@
+// const express = require('express');
+// const app = express();
+// app.use(express.static(__dirname + '/src/index.html')); //aqui você define onde está o index.html da sua aplicação.
+// app.listen(process.env.PORT || 8080);
+
+
 const express = require('express');
+const path = require('path');
+
 const app = express();
-app.use(express.static(__dirname + '/src/index.html')); //aqui você define onde está o index.html da sua aplicação.
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/personal-api-riot'));
+
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/personal-api-riot/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
