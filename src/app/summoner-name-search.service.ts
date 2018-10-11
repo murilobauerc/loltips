2b13021deb58f4529d3fc5178ec5ba24f2e066b4
murilobauerc <panchobauer@gmail.com>
2018-10-11 16:52:15 -0300

    FINALLY WORKING! DEPLOYING TO HEROKU FINAL VERSION

--- src/app/summoner-name-search.service.ts
+++ src/app/summoner-name-search.service.ts
@@ -13,7 +13,7 @@ export class UserSearchService {
   constructor(private http: Http) { }
 
   getUser(summonerName): Observable<any> {
-    let query = summonerName + '?api_key=RGAPI-d85302a9-e324-43e7-8dc1-ea00c33e0539';
+    let query = summonerName + '?api_key=RGAPI-dd608d48-fee5-4e5b-8d35-98580297ab53';
     console.log(this.url + query);
     return this.http.get(this.url + query).pipe(map((res: Response) => res.json()));
   }
