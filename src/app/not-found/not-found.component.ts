fd3027c359dbd530a4b62ba3ebc5f09f1c20f594
murilobauerc <panchobauer@gmail.com>
2018-12-29 21:37:48 -0200

    :rocket: deploying a new version

--- src/app/not-found/not-found.component.ts
+++ src/app/not-found/not-found.component.ts
@@ -3,11 +3,6 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'not-found-html',
     templateUrl: './not-found.component.html',
-    // template:`
-    //     <div class="jumbotron text-center">
-    //         <p> 404. Usuario não encontrado. </p>
-    //     </div>
-    //         `
     styleUrls: ['./not-found.css']
 })
 
--- src/app/summoner-name-search.service.ts
+++ src/app/summoner-name-search.service.ts
@@ -13,7 +13,7 @@ export class UserSearchService {
   constructor(private http: Http) { }
 
   getUser(summonerName): Observable<any> {
-    let query = summonerName + '?api_key=RGAPI-dd608d48-fee5-4e5b-8d35-98580297ab53';
+    let query = summonerName + '?api_key=RGAPI-2ad05484-5ed8-42b3-b8dc-cfa62fff9018';
     console.log(this.url + query);
     return this.http.get(this.url + query).pipe(map((res: Response) => res.json()));
   }
