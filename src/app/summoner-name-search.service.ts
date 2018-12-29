c44d5d584f4733b66b5d719dfe8413a77a886de4
murilobauerc <panchobauer@gmail.com>
2018-12-29 21:46:58 -0200

    :art:

--- src/app/summoner-name-search.service.ts
+++ src/app/summoner-name-search.service.ts
@@ -13,7 +13,7 @@ export class UserSearchService {
   constructor(private http: Http) { }
 
   getUser(summonerName): Observable<any> {
-    let query = summonerName + '?api_key=RGAPI-2ad05484-5ed8-42b3-b8dc-cfa62fff9018';
+    let query = summonerName + '?api_key=RGAPI-b6bffbc4-30eb-482f-8a71-1a4d7076c50a';
     console.log(this.url + query);
     return this.http.get(this.url + query).pipe(map((res: Response) => res.json()));
   }
